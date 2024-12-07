43a8c6249c0de7382a14d8b91bf2b320bd02fa14
fabiandresss <fabianarmijo610@gmail.com>
2024-12-07 14:56:01 -0300

    feat: implementar botones en app.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import './components/Header/Header.css';
 import PhotoSection from './components/PhotoSection/PhotoSection';
 import './components/PhotoSection/PhotoSection.css';
 import Button from './components/Button/Button';
+import './components/Button/Button.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <PhotoSection />
+      <div className="buttons-container1">
+        <Button className="button-pink" text="Reproducir" />
+      </div>
+      <div className="buttons-container2">
+        <Button className="button-pink" text="Más información" />
+      </div>
     </div>
 
 );
